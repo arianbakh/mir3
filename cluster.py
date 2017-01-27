d255810a2c51ecdaf568c3062a271b8fe073eb4a
Arian Bakhtiarnia <arian@cafebazaar.ir>
2017-01-27 19:56:53 +0330

    Find title for clusters

--- cluster.py
+++ cluster.py
@@ -1,3 +1,5 @@
+import math
+
 from elasticsearch.helpers import scan, bulk
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -5,33 +7,32 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from settings import ES, INDEX_NAME, DOC_TYPE, K_MEANS_ACCEPTABLE_DIFF, K_MEANS_RETRY, VERY_HIGH_INERTIA
 
 
-def _get_all_documents():
+def _get_documents_and_features():
     documents = {
         'ids': [],
         'texts': [],
         'vectors': [],
-        'sources': [],  # for debugging
     }
+    features = set()
     for document in scan(ES, query={'query': {'match_all': {}}}, index=INDEX_NAME, doc_type=DOC_TYPE):
         documents['ids'].append(document['_id'])
-
-        documents['sources'].append(document['_source'])
 
         text = document['_source']['analyzed_title'] + ' ' + \
             document['_source']['analyzed_introduction'] + ' ' + \
             document['_source']['analyzed_content']
         documents['texts'].append(text)
 
+        for token in document['_source']['analyzed_title'].split() + document['_source']['analyzed_introduction'].split():
+            features.add(token)
+
     vectorizer = TfidfVectorizer(analyzer='word', min_df=0)
     documents['vectors'] = vectorizer.fit_transform(documents['texts'])
 
-    return documents
+    return documents, list(features)
 
 
-def _get_cluster_update_operations(k_limit):
+def _get_k(k_limit, documents):
     k_limit = int(k_limit)
-
-    documents = _get_all_documents()
 
     k = 1
     last_inertia_average = VERY_HIGH_INERTIA
@@ -52,7 +53,10 @@ def _get_cluster_update_operations(k_limit):
             k += 1
     k -= 1
     print('selected k = %d;' % k)
+    return k
 
+
+def _get_cluster_update_operations(k, documents):
     kmeans = KMeans(n_clusters=k).fit(documents['vectors'])
     for index, label_id in enumerate(kmeans.labels_.tolist()):
         yield {
@@ -66,9 +70,130 @@ def _get_cluster_update_operations(k_limit):
         }
 
 
-def _bulk_action(k_limit):
-    bulk(ES, _get_cluster_update_operations(k_limit), stats_only=False, chunk_size=100)
+def _get_mutual_information(feature, label_id):
+    """
+    http://nlp.stanford.edu/IR-book/html/htmledition/mutual-information-1.html
+    """
+    n11 = ES.count(index=INDEX_NAME, body={
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'multi_match': {
+                            'query': feature,
+                            'fields': ['analyzed_title', 'analyzed_introduction', 'analyzed_content'],
+                        },
+                    },
+                    {
+                        'term': {
+                            'cluster.id': label_id,
+                        },
+                    },
+                ],
+            },
+        },
+    })['count']
+    n10 = ES.count(index=INDEX_NAME, body={
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'multi_match': {
+                            'query': feature,
+                            'fields': ['analyzed_title', 'analyzed_introduction', 'analyzed_content'],
+                        },
+                    },
+                ],
+                'must_not': [
+                    {
+                        'term': {
+                            'cluster.id': label_id,
+                        },
+                    },
+                ],
+            },
+        },
+    })['count']
+    n01 = ES.count(index=INDEX_NAME, body={
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'term': {
+                            'cluster.id': label_id,
+                        },
+                    },
+                ],
+                'must_not': [
+                    {
+                        'multi_match': {
+                            'query': feature,
+                            'fields': ['analyzed_title', 'analyzed_introduction', 'analyzed_content'],
+                        },
+                    },
+                ],
+            },
+        },
+    })['count']
+    n00 = ES.count(index=INDEX_NAME, body={
+        'query': {
+            'bool': {
+                'must_not': [
+                    {
+                        'multi_match': {
+                            'query': feature,
+                            'fields': ['analyzed_title', 'analyzed_introduction', 'analyzed_content'],
+                        },
+                    },
+                    {
+                        'term': {
+                            'cluster.id': label_id,
+                        },
+                    },
+                ],
+            },
+        },
+    })['count']
+    n1x = float(n10 + n11) or 1.0
+    nx1 = float(n01 + n11) or 1.0
+    n0x = float(n00 + n01) or 1.0
+    nx0 = float(n00 + n10) or 1.0
+    n = float(n00 + n01 + n10 + n11) or 1.0
+
+    result = 0
+    try:
+        result += (n11 / n) * math.log2((n * n11) / (n1x * nx1))
+    except ValueError:
+        pass
+    try:
+        result += (n01 / n) * math.log2((n * n01) / (n0x * nx1))
+    except ValueError:
+        pass
+    try:
+        result += (n10 / n) * math.log2((n * n10) / (n1x * nx0))
+    except ValueError:
+        pass
+    try:
+        result += (n00 / n) * math.log2((n * n00) / (n0x * nx0))
+    except ValueError:
+        pass
+    return result
+
+
+def _get_cluster_labels(k, features):
+    print('finding cluster labels...')
+    cluster_to_labels = {}
+    for i in range(k):
+        cluster_to_labels[i] = []
+        for feature in features:
+            cluster_to_labels[i].append([_get_mutual_information(feature, i), feature])
+    for cluster_id, possible_cluster_labels in cluster_to_labels.items():
+        labels = [item[1] for item in sorted(possible_cluster_labels, key=lambda x: -x[0])[:5]]
+        print('cluster_id = %s;' % cluster_id, 'labels = %s;' % str(labels))
 
 
 def cluster(k_limit):
-    _bulk_action(k_limit)
+    documents, features = _get_documents_and_features()
+    k = _get_k(k_limit, documents)
+    bulk(ES, _get_cluster_update_operations(k, documents), stats_only=False, chunk_size=100)
+    _get_cluster_labels(k, features)
--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ from elasticsearch import Elasticsearch
 
 
 OUT_DEGREE = 10
-MAX_PAGES = 100  # TODO 1000
+MAX_PAGES = 100
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 PAGES_DIR = os.path.join(BASE_DIR, 'pages')
 
@@ -17,6 +17,6 @@ INDEX_NAME = 'mir3'
 DOC_TYPE = 'wiki'
 
 
-K_MEANS_ACCEPTABLE_DIFF = 2  # TODO 1
+K_MEANS_ACCEPTABLE_DIFF = 1
 K_MEANS_RETRY = 5
 VERY_HIGH_INERTIA = sys.float_info.max
